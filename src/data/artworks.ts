6bf40b7e692c8c6f1fe3743a939098b4b318990c
mertcalkan <mert.calkan@std.yeditepe.edu.tr>
2025-08-06 16:52:34 +0300

    new images added

--- src/data/artworks.ts
+++ src/data/artworks.ts
@@ -1,5 +1,6 @@
 import artAquarium from "@/assets/Aquarium.jpg";
-import artBear from "@/assets/Bear.jpg";
+import artBear from "@/assets/ayıcık.png";
+import artPerlinNoise from "@/assets/PerlinNoise.png";
 import artBpLogo from "@/assets/BpLogo.jpg";
 import artFlower from "@/assets/flower.png";
 import artFractal from "@/assets/fractal.jpg";
@@ -7,14 +8,14 @@ import artTriangle from "@/assets/triangle.jpg"
 import artShader from "@/assets/Shader.jpg";
 import artMonaLisa from "@/assets/mona lisa.png";
 
-export const artworks = [
-  { id: 1, image: artAquarium, alt: "Concentric circles algorithmic art" },
-  { id: 2, image: artBear, alt: "Purple spiral vortex algorithmic art" },
-  { id: 3, image: artBpLogo, alt: "Green radial lines algorithmic art" },
-  { id: 4, image: artFlower, alt: "Yellow flower pattern algorithmic art" },
-  { id: 5, image: artFractal, alt: "Geometric mandala algorithmic art" },
-  { id: 6, image: artTriangle, alt: "Geometric mandala algorithmic art" },
-  { id: 7, image: artShader, alt: "Geometric mandala algorithmic art" },
-  { id: 7, image: artMonaLisa, alt: "Geometric mandala algorithmic art" }
 
+export const artworks = [
+  { id: 1, image: artAquarium, alt: "Döngü halinde oluşturulmuş özdeş dairelerden oluşan dijital su altı kompozisyonu" },
+  { id: 2, image: artBear, alt: "Öğrencimiz Ege'nin sadece elips şekli kullanarak yaptığı sevimli ayıcık" },
+  { id: 3, image: artBpLogo, alt: "Yeşil ve sarı ışınlardan oluşan radyal çizgilerle tasarlanmış logo" },
+  { id: 4, image: artFlower, alt: "Küçük daire topların spiral renk dağılımı" },
+  { id: 5, image: artPerlinNoise, alt: "Perlin noise: dijital doğa dokusu, ritmik düzensizlik." },
+  { id: 6, image: artTriangle, alt: "Dik Üçgen formlardan oluşan sanatsal tablo " },
+  { id: 7, image: artShader, alt: "Shader algoritmalarıyla oluşturulmuş dinamik ve ışıkla oynayan geometrik mandala yapısı" },
+  { id: 8, image: artMonaLisa, alt: "Mona Lisa portresinin pikselizasyonuyla elde edilmiş tablo." }
 ];
